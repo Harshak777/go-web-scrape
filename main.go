29714b2338c62e42d5733bab0566bff4a27553ba
Harshak777 <harshakkrishnaa777@gmail.com>
2024-02-23 11:42:54 -0700

    Implemented onHTML callback

--- main.go
+++ main.go
@@ -7,7 +7,15 @@ import (
 	"github.com/gocolly/colly"
 )
 
+// defining a data structure to store the scraped data
+type PokemonProduct struct {
+	url, image, name, price string
+}
+
 func main() {
+	// initializing the slice of structs that will contain the scraped data
+	var pokemonProducts []PokemonProduct
+
 	c := colly.NewCollector()
 	c.Visit("https://scrapeme.live/shop/")
 
@@ -23,9 +31,18 @@ func main() {
 		fmt.Println("Page visited: ", r.Request.URL)
 	})
 
-	c.OnHTML("a", func(e *colly.HTMLElement) {
+	c.OnHTML("li.product", func(e *colly.HTMLElement) {
 		// printing all URLs associated with the a links in the page
 		fmt.Printf("%v\n", e.Attr("href"))
+
+		pokemonProduct := PokemonProduct{}
+
+		pokemonProduct.url = e.ChildAttr("a", "href")
+		pokemonProduct.image = e.ChildAttr("img", "src")
+		pokemonProduct.name = e.ChildText("h2")
+		pokemonProduct.price = e.ChildText(".price")
+
+		pokemonProducts = append(pokemonProducts, pokemonProduct)
 	})
 
 	c.OnScraped(func(r *colly.Response) {
